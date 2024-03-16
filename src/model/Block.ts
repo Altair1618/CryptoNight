b68cb3ab3f58fab1b40b89d555e4069977046dfb
Altair1618 <farhannabilsuryono3@gmail.com>
2024-03-16 11:30:36 +0700

    fix: update block to support half size

--- src/model/Block.ts
+++ src/model/Block.ts
@@ -2,15 +2,15 @@ import { BLOCK_SIZE_BYTE } from "@/lib/constants";
 
 export class Block {
   // Container for a block with Block Size of BLOCK_SIZE_BYTE
-  private data: Uint8Array;
+  // Default is 16 bytes (128 bits)
+  // Can be set to 8 bytes (64 bits) by passing isHalf = true for Feistel Network Processing
+  private data: Uint8Array = new Uint8Array();
 
-  constructor(data: Uint8Array | string) {
+  constructor(data: Uint8Array | string, isHalf = false) {
     if (data instanceof Uint8Array) {
-      if (data.length !== BLOCK_SIZE_BYTE) throw new Error('Block size must be 16 bytes');
-      this.data = data;
+      this.setData(data, isHalf);
     } else if (typeof data === 'string') {
-      if (data.length !== BLOCK_SIZE_BYTE * 2) throw new Error('Hex string must be 32 characters');
-      this.data = Buffer.from(data, 'hex');
+      this.setHexData(data, isHalf);
     } else {
       throw new Error('Data must be Uint8Array or hex string');
     }
@@ -28,23 +28,32 @@ export class Block {
     return Buffer.from(this.data).toString('utf8');
   }
 
-  setData(data: Uint8Array): void { 
-    if (data.length !== BLOCK_SIZE_BYTE) {
-      throw new Error('Block size must be 16 bytes');
+  setData(data: Uint8Array, isHalf = false): void { 
+    if (isHalf) {
+      if (data.length !== BLOCK_SIZE_BYTE / 2) throw new Error('Block size must be 8 bytes');
+    } else {
+      if (data.length !== BLOCK_SIZE_BYTE) throw new Error('Block size must be 16 bytes');
     }
 
     this.data = data;
   }
 
-  setHexData(hexData: string): void {
+  setHexData(hexData: string, isHalf = false): void {
+    if (isHalf) {
+      if (hexData.length !== BLOCK_SIZE_BYTE) throw new Error('Hex string must be 16 characters');
+    } else {
+      if (hexData.length !== BLOCK_SIZE_BYTE * 2) throw new Error('Hex string must be 32 characters');
+    }
+
     this.data = Buffer.from(hexData, 'hex');
   }
 
   // Bitwise XOR operation
   xor(block: Block): Block {
-    const result = new Uint8Array(BLOCK_SIZE_BYTE);
-    
-    for (let i = 0; i < BLOCK_SIZE_BYTE; i++) {
+    const size = this.data.length;
+    const result = new Uint8Array(size);
+
+    for (let i = 0; i < size; i++) {
       result[i] = this.data[i] ^ block.getData()[i];
     }
 
