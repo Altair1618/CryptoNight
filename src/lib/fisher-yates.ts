6c169791d1e9dfa1d048a0786ba6ab2f9c87734b
Altair1618 <farhannabilsuryono3@gmail.com>
2024-03-16 12:58:24 +0700

    fix: swap the bit instead of byte in fisher yates

--- src/lib/fisher-yates.ts
+++ src/lib/fisher-yates.ts
@@ -1,19 +1,50 @@
 import { Block } from "@/model/Block";
 import seedrandom from "seedrandom";
 
-export function fisherYatesShuffler(data: Block, seed: Block): Block {
+function getBit(data: Block, i: number): number {
+  const temp = data.getData()[Math.floor(i / 8)];
+  return (temp >> (8 - (i % 8) - 1)) & 1;
+}
+
+function swapBit(data: Block, i: number, j: number): void {
+  const bitI = getBit(data, i);
+  const bitJ = getBit(data, j);
+  
+  if (bitI !== bitJ) {
+    data.getData()[Math.floor(i / 8)] ^= 1 << (8 - (i % 8) - 1);
+    data.getData()[Math.floor(j / 8)] ^= 1 << (8 - (j % 8) - 1);
+  }
+
+  return;
+}
+
+export function fisherYatesShuffler(
+  data: Block,
+  seed: Block,
+  isDeshuffle: boolean
+): Block {
+  // Fisher-Yates Shuffler to Permute the Data Bits
   const rng = seedrandom(seed.getHexData());
   const length = data.getData().length;
-  const result = new Uint8Array(length);
+  const result = new Block(data.getHexData(), length === 8);
 
-  for (let i = 0; i < length; i++) {
-    result[i] = data.getData()[i];
+  // Generate pseudo-random list to shuffle the data
+  const randomList = new Array<number>(8 * length);
+  
+  randomList[0] = 0; // Should not be used in the shuffle process
+  for (let i = 8 * length - 1; i > 0; i--) {
+    randomList[i] = Math.floor(rng() * (i + 1));
   }
 
-  for (let i = length - 1; i > 0; i--) {
-    const j = Math.floor(rng() * (i + 1));
-    [result[i], result[j]] = [result[j], result[i]];
+  if (isDeshuffle) {
+    for (let i = 1; i < 8 * length; i++) {
+      swapBit(result, i, randomList[i]);
+    }
+  } else {
+    for (let i = 8 * length - 1; i > 0; i--) {
+      swapBit(result, i, randomList[i]);
+    }
   }
 
-  return new Block(result, data.getData().length === 8);
+  return result;
 }
