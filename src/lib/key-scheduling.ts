e1e53dab9ef69a21eb2551a37c6aa9577091ae2c
AustinPardosi <gabrielpardosi26@gmail.com>
2024-03-20 21:47:08 +0700

    fix: fixing key-sheduling

--- src/lib/key-scheduling.ts
+++ src/lib/key-scheduling.ts
@@ -4,14 +4,19 @@ import { fisherYatesShuffler } from "./fisher-yates";
 export function generateRoundKeys(key: Block, rounds: number): Block[] {
   const keys = new Array<Block>(rounds);
 
-  let currentKey = key.getHexData();
+  // Set initial key for 1st round
+  keys[0] = key;
+
+  // Iteration for key every round
+  // Using seed from prev key to output newest key
   for (let i = 0; i < rounds; i++) {
-    currentKey = fisherYatesShuffler(
-      new Block(currentKey, true),
-      key,
+    let seed = keys[i - 1];
+    let currentKeyData = fisherYatesShuffler(
+      new Block(seed.getHexData(), true),
+      seed,
       true
     ).getHexData();
-    keys[i] = new Block(currentKey, true);
+    keys[i] = new Block(currentKeyData, true);
   }
 
   return keys;
